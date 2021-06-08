6bf115a666ea9b0fb821656673d34e9b0723b8c4
Jim Belton <jim.belton@gmail.com>
2021-06-07 22:42:10 -0700

    Benchmark cJSON parsing

--- json-shootout.c
+++ json-shootout.c
@@ -1,10 +1,33 @@
 #include <assert.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <cjson/cJSON.h>
 #include <fcntl.h>
+#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+
+// install cJSON with: sudo apt install libcjson-dev
+// build with: cc json-shootout.c -lcjson
+
+static double
+doubletime(void)
+{
+    struct timeval now;
+
+    assert(gettimeofday(&now, NULL) == 0);
+    return now.tv_sec + now.tv_usec / 1000000.0;
+}
+
+static size_t
+memory(void)
+{
+    struct mallinfo info = mallinfo();
+
+    return info.uordblks;
+}
 
 int
 main(void)
@@ -20,7 +43,16 @@ main(void)
     assert(read(file, json, stats->st_size) == stats->st_size);
     json[stats->st_size] = '\0';
 
-    fprintf(stderr, "About to dump %zu byte file contents\n", (size_t)stats->st_size);
-    write(1, json, stats->st_size);
+    cJSON *cjson;
+    double startTime, duration;
+    size_t startMem,  usedMem;
+
+    startMem  = memory();
+    startTime = doubletime();
+    assert(cjson = cJSON_Parse(json));
+    duration = doubletime() - startTime;
+    usedMem  = memory()     - startMem;
+
+    printf("Loaded dictionary in %fs, size %zukB\n", duration, usedMem / 1000);
     return 0;
 }
